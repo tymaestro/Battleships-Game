15094b458770014c8e4b0ec94be77bda85af6119
tymaestro <tymaestro22@gmail.com>
2021-12-09 16:38:31 +0000

    write board initialization

--- run.py
+++ run.py
@@ -3,4 +3,13 @@
 from random import randint
 
 Class Board:
-    """"""
+    """
+    Board initialization. Creates new board including size, number of ships
+    and position of ships
+    """
+
+    def __init__(self, size, num_ships, name, type):
+        self.size = size
+        self.num_ships = num_ships
+        self.name = name
+        self.type = type
