a9743b4d37728a2a50650b32b0833c529aad4c4b
tymaestro <tymaestro22@gmail.com>
2021-12-10 11:00:30 +0000

    add print message to new game

--- run.py
+++ run.py
@@ -2,26 +2,35 @@
 
 from random import randint
 
-Class Board:
-    """
-    Board initialization. Creates new board including size, number of ships
-    and position of ships
-    """
+scores = {"computer": 0, "player": 0}
 
-    def __init__(self, size, num_ships, name, type):
-        self.size = size
-        self.num_ships = num_ships
-        self.name = name
-        self.type = type
-        self.ships = []
-        self.guesses = []
+# Class Board:
+#     """
+#     Board initialization. Creates new board including size, number of ships
+#     and position of ships
+#     """
+
+#     def __init__(self, size, num_ships, name, type):
+#         self.size = size
+#         self.num_ships = num_ships
+#         self.name = name
+#         self.type = type
+#         self.ships = []
+#         self.guesses = []
+
+board = []
+
+def create_board():
+    for x in range(8)
 
 def new_game():
+    print("Ahoy Matey! Welcome to Battleships!")
     size = 8
     num_ships = 5
     scores["computer"] = 0
     scores["player"] = 0
     player_name = input("Please enter your name")
+    print(new_game())
 
 
 new_game()
