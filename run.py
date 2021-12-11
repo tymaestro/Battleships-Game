8acc458bbaf0d35a4dd71097a0a158307eca3580
tymaestro <tymaestro22@gmail.com>
2021-12-11 18:07:40 +0000

    add print board function, description, and for loop

--- run.py
+++ run.py
@@ -9,3 +9,12 @@ Create board
 """
 
 board = [["-"] * 6 for x in range(6)]
+
+
+def print_board(board):
+    """
+    print_board function prints board and adds a pipe
+    between each hyphen for clarity
+    """
+    for row in board:
+        print(" | ".join(row))
