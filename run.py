57e859899ee359061c8b0b83e55cbf2056cb3bdf
tymaestro <tymaestro22@gmail.com>
2021-12-14 20:03:36 +0000

    add docstrings to all functions, edit score function, introduce while loop to repeat game

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-# Import randint from random module
+""" Import randint from random module """
 
 from random import randint
 
@@ -24,8 +24,6 @@ def print_board(board):
         print(" | ".join(row))
 
 
-# Collecting user input for name and printing opening message
-
 def welcome(player):
     """
     welcome message for players
@@ -33,40 +31,39 @@ def welcome(player):
     print(f"Welcome to Battleships {player}!")
 
 
-# generate random ship locations
-
 def ship_placement(board):
     """
     Generate random ship locations
     """
-    for ship in range(6):
+    for ship in range(5):
         ship_row, ship_column = randint(0, 5), randint(0, 5)
         board[ship_row][ship_column] = "X"
-
-# Player guesses
 
 
 def player_guess():
     """
     Player guesses
     """
-    # try:
-    row = int(input("Please enter a row number: "))
-    while row < 0 or row > 5:
-        print("Oops, please choose a row between 0 and 5!")
+    try:
         row = int(input("Please enter a row number: "))
-    column = int(input("Please enter a column number: "))
-    while column < 0 or column > 5:
-        print("Oops, please choose a column between 0 and 5!")
+        while row < 0 or row > 5:
+            print("Oops, please choose a row between 0 and 5!")
+            row = int(input("Please enter a row number: "))
         column = int(input("Please enter a column number: "))
-    # except ValueError:
-    #     print("Please enter a valid number!")
-    #     row = int(input("Please enter a row number: "))
+        while column < 0 or column > 5:
+            print("Oops, please choose a column between 0 and 5!")
+            column = int(input("Please enter a column number: "))
+    except ValueError:
+        print("Please enter a valid number!")
+        row = int(input("Please enter a row number: "))
     print(f"You chose the co-ordinates ({row}, {column})")
     return int(row), int(column)
 
 
 def guess_check():
+    """
+    Function to validate player guesses
+    """
     row, column = player_guess()
     if computer_board[row][column] == "#":
         print("Oops, you've already guessed those co-ordinates!")
@@ -78,10 +75,24 @@ def guess_check():
         computer_board[row][column] = "#"
 
 
-# Main start function for the game
+def increment_score(board):
+    """
+    Function to increment the score by 1 each time a ship is hit
+    """
+    score = 0
+    for row in board:
+        for column in row:
+            if column == "X":
+                score += 1
+    print(f"You've hit {score} out of 5 battleships")
+    return score
 
 
 def start_game():
+    """
+    Start game function which calls the ship_placement function
+    asks for name input, and calls welcome function
+    """
     ship_placement(player_board)
     name = input("Ahoy Matey!\nPlease enter your name to continue: ")
     welcome(name)
@@ -92,13 +103,18 @@ def main():
     This is the main function for starting the game
     """
     start_game()
-    print("Please choose co-ordinates between 0 and 5")
-    print("\n\nPlayer Board\n")
-    print_board(player_board)
-    print("\n\nComputer Board\n")
-    print_board(computer_board)
-    print("\n")
-    guess_check()
+    score = 5
+    while score < 6:
+        print("Please choose co-ordinates between 0 and 5")
+        # print("\n\nPlayer Board\n")
+        # print_board(player_board)
+        print("\n\nComputer Board\n")
+        print_board(computer_board)
+        print("\n")
+        guess_check()
+        if increment_score(computer_board) == 5:
+            print("Congratulations! You sunk all the Battleships!")
+            break
 
 
 if __name__ == "__main__":
